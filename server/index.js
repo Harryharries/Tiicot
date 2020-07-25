353aca2e1347cc4d9d3aa86478a3361e84297eb6
Harryharries <a1021604754@gmail.com>
2020-07-24 23:42:16 -0500

    add backend

--- server/index.js
+++ server/index.js
@@ -49,12 +49,12 @@ app.get('/student/:name', async(req, res) => {
     res.send(JSON.stringify(resultList));
 })
 
-// local
-app.listen(3001,()=>{
-    console.log('success')
-})
+// // local
+// app.listen(3001,()=>{
+//     console.log('success')
+// })
 
-// const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-// app.listen(port, function () {
-//     console.log('Server listening on port ' + port);
-// });
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3001;
+app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
